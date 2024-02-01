1a52d86ca73b795a2f66de97cd0c98aac37b60e2
vadi <vtaslim@solutiondigital.biz>
2024-02-01 13:50:02 +1100

    updated next version

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   reactStrictMode: false,
   basePath: '/base-path',
   assetPrefix: `/base-path/`,
-  generateEtags: false,
-  poweredByHeader: false,
-  trailingSlash: false,
-  compress: true,
-  inlineImageLimit: false,
-  reactStrictMode: true,
-  swcMinify: false,
   i18n: {
     locales: ['en_US'],
     defaultLocale: 'en_US'
